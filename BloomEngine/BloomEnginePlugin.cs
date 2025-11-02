f492d8818b7d9269f3041cb0daeaf2c860846226
PalmForest <realpalmforest@gmail.com>
2025-11-02 18:03:24 +1100

    Add ingame plugin list

---
+++ BloomEngine/BloomEnginePlugin.cs
@@ -0,0 +1,27 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+
+namespace BloomEngine;
+
+[BepInPlugin(Name, Name, Version)]
+[BepInProcess("Replanted.exe")]
+public class BloomEnginePlugin : BasePlugin
+{
+    public const string Name = "BloomEngine";
+    public const string Version = "1.0.0";
+    public const string Id = "com.palmforest.bloomengine";
+
+    public Harmony Harmony { get; } = new Harmony(Id);
+
+    public static ManualLogSource Logger;
+
+    public override void Load()
+    {
+        Logger = Log;
+        Logger.LogInfo($"Successfully initialised {nameof(BloomEngine)}.");
+
+        Harmony.PatchAll();
+    }
+}
